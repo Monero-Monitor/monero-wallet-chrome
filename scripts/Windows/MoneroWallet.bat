060bb122431fa6ff7ba394fd521b81ef8f02c3c1
Mike C <micro-machine@users.noreply.github.com>
2016-04-26 23:35:30 -0600

    Bring Windows launch script in line with Unix scripts
    Untested. Use at your own risk.

--- scripts/Windows/MoneroWallet.bat
+++ scripts/Windows/MoneroWallet.bat
@@ -9,17 +9,49 @@ REM This script is released under the MIT License.
 
 @echo off
 cls
-set FILE=ChromeWallet.bin
+set WALLET_FILENAME=wallet.bin
+set PROGRAM_DIR=C:\Program Files\MoneroWallet
 
 echo.
 echo ====================================================================================
-echo                             Monero Simplewallet Launcher
+echo                       Monero Simplewallet Launcher for Windows
 echo.
 echo Copyright (c) 2016 bigreddmachine.
 echo Released under the MIT License as part of Monero Wallet for Chrome.
 echo https://github.com/monero-monitor/monero-wallet-chrome
 echo ====================================================================================
 echo.
+
+set WALLET_DIR=%PROGRAM_DIR%\wallet
+IF not exist %WALLET_DIR% (mkdir %WALLET_DIR%)
+set FILE=%WALLET_DIR%\%WALLET_FILENAME%
+
+echo Which Monero daemon would you like to connect to?
+echo   1. remote (node.moneroclub.com:8880)
+echo   2. local  (127.0.0.1:18081)
+echo   3. custom
+echo.
+set /p DAEMON=Please make a selection: 
+
+if NOT %DAEMON% == 1 if NOT %DAEMON% == 2 if NOT %DAEMO% == 3 (
+    set -p exitinput=Invalid daemon selection. Press enter to quit.
+    exit
+)
+
+if %DAEMON% == 1 (
+    set USE_DAEMON=node.moneroclub.com:8880
+)
+if %DAEMON% == 2 (
+    set USE_DAEMON=127.0.0.1:18081
+)
+if %DAEMON% == 3 (
+    set /p USE_DAEMON=Please enter custom daemon (ex: 192.168.0.1:18081): 
+)
+
+echo.
+echo ----------------------
+echo.
+
 
 if exist %FILE% (
     REM If this script has been run previously, a wallet should already exist. Start it in RPC mode.
@@ -28,33 +60,21 @@ if exist %FILE% (
     echo.
     set /p PASS=Please enter your password: 
     
-    start "simplewallet --wallet-file %FILE% --password %PASS% --rpc-bind-ip 127.0.0.1 --rpc-bind-port 18082"
-    
     echo Launching simplewallet...
     echo.
-    sleep 3
-    echo ----------------------
     
+    start "%PROGRAM_DIR%\simplewallet --wallet-file %FILE% --password %PASS% --rpc-bind-ip 127.0.0.1 --rpc-bind-port 18082 --daemon-address %USE_DAEMON%"
+    
+    echo Simplewallet should now be running in RPC mode. You can verify this by checking if
+    echo your Chrome extension says "online". It may take a few seconds for the wallet to
+    echo connect to simplewallet. If it fails to come online, please check your password
+    echo and try again.
     echo.
-    echo Returning wallet address:
+    echo To shutdown simplewallet, choose 'Stop Wallet' in the extension's menu.
+    echo.
+    echo Thank you for using Monero Wallet for Chrome! You may now close this window.
+    echo.
     
-    if (curl --fail -X POST http://127.0.0.1:18082/json_rpc -d '{"jsonrpc":"2.0","id":"0","method":"getaddress"}' -H 'Content-Type: application/json'; then
-        
-        echo.
-        echo.
-        echo Simplewallet is now running in RPC mode. To shutdown simplewallet, choose 'Stop Wallet' in the extension's menu.
-        echo.
-        echo Thank you for using Monero Wallet for Chrome! You may now close this window.
-        echo.
-        
-    ) else (
-        
-        echo.
-        echo Please check if your extension shows that simplewallet online. If not,
-        echo please check your password and try again.
-        echo.
-        
-    )
 ) else (
     REM The first time this script is run, open the command line wallet.
     
@@ -73,20 +93,32 @@ if exist %FILE% (
     echo.
     echo ----------
     set /p NEWPASS=Please pick a password for your new wallet: 
+    echo.
+    set /p CONFIRM_NEWPASS=Confirm password: 
+    if NOT %NEWPASS% == %CONFIRM_NEWPASS% (
+        echo Your passwords do not match. Please restart the script and try again.
+        echo.
+        set /p userinput=Press "enter" to exit.
+        exit
+    ) 
     echo ----------
     echo.
     
-    simplewallet --generate-new-wallet %FILE% --password %NEWPASS%
+    %PROGRAM_DIR%\simplewallet --generate-new-wallet %FILE% --password %NEWPASS%  --daemon-address %USE_DAEMON%
     
     echo.
     echo ----------
     echo.
     
     if exist %FILE% (
-        echo   Everything looks good. You can now start your wallet in RPC mode at any time by re-running this script.
+        echo Everything looks good. You can now start your wallet in RPC mode at any time by re-running this script.
         echo.
-        echo   Closing Monero Simplewallet Launcher...
+        set /p userinput=Press "enter" to exit Monero Simplewallet Launcher.
+    ) else (
+        echo There seems to have been an issue creating your wallet.
         echo.
-        echo.
+        set /p userinput=Press "enter" to exit.
     )
 )
+
+echo.
