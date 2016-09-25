c86df24ac0d3f6db25fd0d775abb725408a89fb5
Mike C <bigreddmachine@users.noreply.github.com>
2016-09-25 11:01:54 -0600

    Add option to save User Agent in wallet
    Users can set a User Agent in the Options page.
    Stored in Chrome local storage.

--- build/chrome/lib/js/pages/options.js
+++ build/chrome/lib/js/pages/options.js
@@ -7,14 +7,20 @@ document.addEventListener('DOMContentLoaded', function () {
 function save_options() {
   // Read settings from Options pop-up box:
   var walletPort = String(document.getElementById('monero-wallet-cli-port').value);
-
+  var saveUserAgent = false, userAgent = '';
+  if (document.getElementById('options-save-user-agent').checked) {
+    var saveUserAgent = true;
+    var userAgent = String(document.getElementById('options-user-agent').value);
+  }
   // Save options in Chrome storage:
   if (Number(walletPort) >= 1 && Number(walletPort) <= 65535) {
     chrome.storage.sync.set({
-      walletPort: walletPort
+      walletPort: walletPort,
+      saveUserAgent: saveUserAgent,
+      userAgent: userAgent
     }, function() {
       var status = document.getElementById('status');
-      updatePort(walletPort);
+      updateWalletInfo(walletPort, saveUserAgent, userAgent);
       status.textContent = 'Options saved.';
       setTimeout(function() {
         status.textContent = '';
@@ -22,8 +28,21 @@ function save_options() {
     });
   } else {
     var status = document.getElementById('status');
-    status.textContent = 'Invalid Port.';
-    document.getElementById('monero-wallet-cli-port').value = 18082;
+    chrome.storage.sync.set({
+      walletPort: '18082',
+      saveUserAgent: saveUserAgent,
+      userAgent: userAgent
+    }, function() {
+      var status = document.getElementById('status');
+      updateWalletInfo(walletPort, saveUserAgent, userAgent);
+      document.getElementById('monero-wallet-cli-port').value = 18082;
+      status.textContent = 'Options saved. Port invalid... set to 18082.';
+      setTimeout(function() {
+        status.textContent = '';
+      }, 2000);
+    });
+
+
     setTimeout(function() {
       status.textContent = '';
       save_options();
@@ -34,9 +53,13 @@ function save_options() {
 function restore_options() {
   // Get saved options from Chrome storage:
   chrome.storage.sync.get({
-    walletPort: ''
+    walletPort: '',
+    saveUserAgent: false,
+    userAgent: ''
   }, function(items) {
     document.getElementById('monero-wallet-cli-port').value = items.walletPort;
+    document.getElementById('options-save-user-agent').checked = items.saveUserAgent;
+    document.getElementById('options-user-agent').value = items.userAgent;
   });
 }
 
@@ -45,6 +68,7 @@ function clear_storage() {
   chrome.storage.sync.clear( function() {
     var status = document.getElementById('status')
     document.getElementById('monero-wallet-cli-port').value = '';
+    document.getElementById('options-save-user-agent').checked = false;
     status.textContent = 'Options cleared';
     setTimeout(function() {
       status.textContent = '';
@@ -52,8 +76,13 @@ function clear_storage() {
   });
 }
 
-function updatePort(port) {
-  var request = {greeting: "Monero monero-wallet-cli Update Wallet Port", newWalletPort: port };
+function updateWalletInfo(port, saveUserAgent, userAgent) {
+  var request = {
+    greeting: "Monero monero-wallet-cli Update Wallet Info",
+    newWalletPort: port,
+    saveUserAgent: saveUserAgent,
+    userAgent: userAgent
+  };
   chrome.runtime.sendMessage(request, function (resp) {
     console.log(resp);
   });
