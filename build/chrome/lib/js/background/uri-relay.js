9643adddc35900355b7ad394520ff461edcc6271
Mike C <bigreddmachine@users.noreply.github.com>
2016-09-25 12:02:16 -0600

    Improved information relays
    - streamline chrome.runtime.onMessage listening
    - pass wallet_info within the extension and not to content scripts.
    - allow send and keys pages to receive full wallet_info so that they
    can talk to monero-wallet-cli

--- build/chrome/lib/js/background/uri-relay.js
+++ build/chrome/lib/js/background/uri-relay.js
@@ -5,21 +5,12 @@ var send_href;
 
 // Listen for a payment request to open a tab to send Monero to a Monero URI
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
   if (request.greeting == "Monero monero-wallet-cli Payment Request") {
-
     send_href = request.href;
-
     var send_tab = chrome.tabs.create({url: '/data/html/send.html'});
-
     sendResponse("Payment Request Received.");
-  }
-});
-
-//
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  // console.log(sender.tab ? "from a content script:" + sender.tab.url : "from the extension");
-  if (request.greeting == "Monero monero-wallet-cli Fill Send Page") {
+  } else if (request.greeting == "Monero monero-wallet-cli Fill Send Page" &&
+      sender.tab.url === "chrome-extension://" + chrome.runtime.id + "/data/html/send.html") {
     sendResponse(send_href);
     deleteProperties(send_href);
   }
